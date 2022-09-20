41f8e3fd855daffe604111515e2bb4c69d971e97
triplem123 <triplem804@gmail.com>
2022-09-20 19:05:20 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Site is going to take some time to take form... bear with me.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
